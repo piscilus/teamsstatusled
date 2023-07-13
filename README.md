543f1fd3931e49de72910d4babd516dcb6abebff
Julian Krämer <piscilus@kxse.de>
2023-07-13 22:17:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# MS Teams Status LED
+
+The user's current availability in Microsoft Teams is indicated by 
+*user presence states*:
+
+- Available
+- Busy
+- DoNotDisturb
+- Away
+- BeRightBack
+- Offline
+
+This tool accesses this information and maps it to a color which is then 
+displayed by a RGB-LED. The current implementation uses 
+[BlinkStick Square](https://www.blinkstick.com/products/blinkstick-square).
+
+## MS Teams
+
+MS Teams logs extensively to `%APPDATA%/Microsoft/Teams/logs.txt`. Every change
+of the *user presence state* can be found there as well. The script simply 
+opens the file on regular basis searching for the latest change starting from 
+the end of the file.
+
+A log entry of interest looks like this:
+
+```text
+Thu Jul 13 2023 18:07:10 GMT+0200 (Central European Summer Time) <12116> -- info -- StatusIndicatorStateService: Added Available (current state: Away -> Available)| profileType: AAD
+```
+
+A regular expression is used to extract the state.
+
+## Libraries
+
+- BlinkStick library for Python 3.x 
+  - <https://pypi.org/project/BlinkStick310/1.0.1/>
+- file_read_backwards
+  - <https://pypi.org/project/file-read-backwards/>
---
+++ src/tsl.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+"""MS Teams Status LED
+
+This tool accesses the current Microsoft Teams user presence state and maps it
+to a color which is then displayed by a RGB-LED.
+"""
+
+__author__ = "<piscilus> Julian Krämer"
+__copyright__ = "Copyright 2023, Julian Krämer"
+__date__ = "2023-07-13"
+__license__ = "MIT"
+__version__ = "0.1.0-alpha"
+
+import argparse
+import contextlib
+import os
+import re
+import sys
+from time import sleep
+
+from blinkstick310 import blinkstick
+from file_read_backwards import FileReadBackwards
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                        prog='tsl',
+                        description='Show MS Teams user presence state with LED',
+                        epilog='Text at the bottom of help')
+    parser.add_argument('-i', '--interval',
+                        type=int,
+                        default=1,
+                        help="Interval in seconds to check state (default: 5)")
+    parser.add_argument('-v', '--verbose',
+                        action='store_true')
+    parser.add_argument('-V' '--version',
+                        action='version', version='%(prog)s ' + __version__)
+    args = parser.parse_args()
+
+    state_colors = {"Offline"       : "#000000",
+                    "Available"     : "#00FF00",
+                    "Busy"          : "#FF7500",
+                    "DoNotDisturb"  : "#FF0000",
+                    "Away"          : "#FFFF00",
+                    "BeRightBack"   : "#0000FF"}
+
+    bstick = blinkstick.find_first()
+    if bstick == None:
+        print("Error: BlinkStick not found!")
+        sys.exit(1)
+
+    bstick.set_led_count(8)
+    bstick.set_mode(0)
+    current_color = bstick.get_color(0, "hex")
+
+    path = os.path.join(os.getenv('APPDATA'), "Microsoft", "Teams", "log.txt")
+    try:
+        while True:
+            state = "Offline"
+            with FileReadBackwards(path, encoding="utf-8") as frb:
+                for line in frb:
+                    match = re.search(r'StatusIndicatorStateService: Added\s+(\w+)', line)
+                    if match:
+                        state = match.group(1)
+                        break
+            with contextlib.redirect_stdout(None) if not args.verbose else contextlib.nullcontext():
+                try:
+                    color = state_colors[state]
+                except KeyError:
+                    color = state_colors["Offline"]
+                if current_color != color:
+                    bstick.set_color(channel=0, index=1, hex=color)
+                    current_color = color
+                    print("change to ", color)
+                else:
+                    print("nop")
+            sleep(args.interval)
+    except FileNotFoundError:
+        print("Error: File not found: %s." % path)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        pass
+    bstick.set_color(channel=0, index=1, hex=state_colors["Offline"])
+    sys.exit(0)
